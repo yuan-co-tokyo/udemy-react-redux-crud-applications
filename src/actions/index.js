fd2e0eeeae5976f81f61da25fc629a31190fe05f
yuan.co.tokyo <yuan.co.tokyo@gmail.com>
2021-01-19 17:13:27 +0900

    learn action

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+export const increment = () => ({
+        type: INCREMENT
+})
+
+eexport const decrement = () => ({
+    type: DECREMENT
+})
