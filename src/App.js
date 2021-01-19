b01c2f17d8548491fe4c635b8f83b5b208bf7516
yuan.co.tokyo <yuan.co.tokyo@gmail.com>
2021-01-19 16:14:24 +0900

    learn jsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import React, {Component} from 'react';
 
-function App() {
-  return <h1>Hello World</h1>;
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={ () => {console.log("I'm clicked!")}} />
+      </div>
+    )
+  }
 }
 
 export default App;
