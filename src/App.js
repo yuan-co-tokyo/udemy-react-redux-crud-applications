e1f6e11abf4bb26b5cc83d605447f968a0c88eed
yuan.co.tokyo <yuan.co.tokyo@gmail.com>
2021-01-19 16:27:15 +0900

    components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={ () => {console.log("I'm clicked!")}} />
-      </div>
-    )
-  }
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={ () => {console.log("I'm clicked!")}} />
+//       </div>
+//     )
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+};
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
